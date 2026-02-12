be2749dc1f190b03b43f3507fb028bf9ebcf3530
MrKalyanKing <badhavathkalyan08@gmail.com>
2026-02-12 20:39:18 +0530

    Server setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+
+
+const app = express()
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+app.get("/", (req, res) => {
+    res.send("Ledger Service is up and running")
+})
+
+module.exports=app
